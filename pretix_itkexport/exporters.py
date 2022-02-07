c6b1b2ba14015e55f4687e5889036a8385b5a9bd
Raphael Michel <michel@rami.io>
2022-02-07 17:28:53 +0100

    Rewrite from orders to orderpayments and orderrefunds

--- pretix_itkexport/exporters.py
+++ pretix_itkexport/exporters.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from django.template.defaultfilters import floatformat
 from django.utils.translation import ugettext_lazy as _
 from pretix.base.models.log import LogEntry
-from pretix.base.models.orders import Order
+from pretix.base.models.orders import Order, OrderPayment, OrderRefund
 from pretix_paymentdibs.payment import DIBS
 
 
@@ -34,22 +34,22 @@ class Exporter():
         return floatformat(amount, 2)
 
     def getData(self, **kwargs):
-        paid_orders = self.loadPaidOrders(**kwargs)
-        refunded_orders = self.loadRefundedOrders(**kwargs)
-        cash_orders = self.loadCashOrders(**kwargs)
-        return self.formatData(paid_orders, refunded_orders, cash_orders, **kwargs)
+        payments = self.loadPayments(**kwargs)
+        refunds = self.loadRefunds(**kwargs)
+        cash_payments = self.loadCashPayments(**kwargs)
+        return self.formatData(payments, refunds, cash_payments, **kwargs)
 
-    def loadPaidOrders(self, **kwargs):
+    def loadPayments(self, **kwargs):
         return []
 
-    def loadRefundedOrders(self, **kwargs):
+    def loadRefunds(self, **kwargs):
         return []
 
-    def loadCashOrders(self, **kwargs):
+    def loadCashPayments(self, **kwargs):
         return []
 
-    def formatData(self, paid_orders, refunded_orders, cash_orders, **kwargs):
-        return paid_orders
+    def formatData(self, payments, refunds, cash_payments, **kwargs):
+        return payments
 
 
 class PaidOrdersExporter(Exporter):
@@ -91,59 +91,49 @@ class PaidOrdersExporter(Exporter):
     index_amount = headers.index('Beløb')
     index_text = headers.index('Tekst')
 
-    def loadPaidOrders(self, **kwargs):
-        order_filter = {
-            'event__organizer__in': self.organizers,
-            'status__in': [Order.STATUS_PAID, Order.STATUS_REFUNDED],
-            'payment_provider': 'dibs',
-            'total__gt': 0
+    def loadPayments(self, **kwargs):
+        payment_filter = {
+            'order__event__organizer__in': self.organizers,
+            'state__in': [OrderPayment.PAYMENT_STATE_CONFIRMED, OrderPayment.PAYMENT_STATE_REFUNDED],
+            'provider': 'dibs',
         }
         if 'starttime' in kwargs:
-            order_filter['payment_date__gte'] = kwargs['starttime']
+            payment_filter['payment_date__gte'] = kwargs['starttime']
         if 'endtime' in kwargs:
-            order_filter['payment_date__lt'] = kwargs['endtime']
+            payment_filter['payment_date__lt'] = kwargs['endtime']
 
-        orders = Order.objects.filter(**order_filter).order_by('payment_date')
+        payments = OrderPayment.objects.filter(**payment_filter).order_by('payment_date').select_related('order', 'order__event')
+        return payments
 
-        return orders
-
-    def loadRefundedOrders(self, **kwargs):
-        # https://docs.djangoproject.com/en/2.0/topics/db/sql/#adding-annotations
-        order_table_name = Order.objects.model._meta.db_table
-        logentry_table_name = LogEntry.objects.model._meta.db_table
-        sql = 'select o.*, l.datetime refund_date from ' + order_table_name + ' o ' \
-            ' inner join ' + logentry_table_name + ' l on l.object_id = o.id and l.action_type = %(action_type)s' \
-            ' where o.status in %(status)s and o.payment_provider = %(payment_provider)s and o.total > 0' \
-            ' and %(starttime)s <= l.datetime and l.datetime < %(endtime)s'
-        # todo: filter by organizer
-        parameters = {
-            'action_type': 'pretix.event.order.refunded',
-            'status': [Order.STATUS_REFUNDED],
-            'payment_provider': 'dibs',
-            'starttime': kwargs['starttime'] if 'starttime' in kwargs else '2001-01-01',
-            'endtime': kwargs['endtime'] if 'endtime' in kwargs else '2087-01-01'
-        }
-        orders = Order.objects.raw(sql, parameters)
-
-        return orders
-
-    def loadCashOrders(self, **kwargs):
-        order_filter = {
-            'event__organizer__in': self.organizers,
-            'status__in': [Order.STATUS_PAID, Order.STATUS_REFUNDED],
-            'payment_provider': 'cash',
-            'total__gt': 0
+    def loadRefunds(self, **kwargs):
+        refund_filter = {
+            'order__event__organizer__in': self.organizers,
+            'state__in': [OrderRefund.REFUND_STATE_DONE],
+            'provider': 'dibs',
         }
         if 'starttime' in kwargs:
-            order_filter['payment_date__gte'] = kwargs['starttime']
+            refund_filter['payment_date__gte'] = kwargs['starttime']
         if 'endtime' in kwargs:
-            order_filter['payment_date__lt'] = kwargs['endtime']
+            refund_filter['payment_date__lt'] = kwargs['endtime']
 
-        orders = Order.objects.filter(**order_filter).order_by('payment_date')
+        refunds = OrderRefund.objects.filter(**refund_filter).order_by('execution_date').select_related('order', 'order__event', 'payment')
+        return refunds
 
-        return orders
+    def loadCashPayments(self, **kwargs):
+        payment_filter = {
+            'order__event__organizer__in': self.organizers,
+            'state__in': [OrderPayment.PAYMENT_STATE_CONFIRMED, OrderPayment.PAYMENT_STATE_REFUNDED],
+            'provider': 'cash',
+        }
+        if 'starttime' in kwargs:
+            payment_filter['payment_date__gte'] = kwargs['starttime']
+        if 'endtime' in kwargs:
+            payment_filter['payment_date__lt'] = kwargs['endtime']
 
-    def formatData(self, paid_orders, refunded_orders, cash_orders, **kwargs):
+        payments = OrderPayment.objects.filter(**payment_filter).order_by('payment_date').select_related('order', 'order__event')
+        return payments
+
+    def formatData(self, payments, refunds, cash_payments, **kwargs):
         raise Exception(self.__class__.__name__+'.formatData not implemented')
 
     def localizeCardType(self, card_type):
@@ -153,58 +143,58 @@ class PaidOrdersExporter(Exporter):
             return _('debit')
         return card_type
 
-    def getCardType(self, order):
-        return DIBS.get_payment_card_type(order) if DIBS.identifier == order.payment_provider else None
+    def getCardType(self, payment):
+        return DIBS.get_payment_card_type(payment) if DIBS.identifier == payment.provider else None
 
 
 class PaidOrdersLineExporter(PaidOrdersExporter):
-    def loadPaidOrders(self, **kwargs):
-        orders = super().loadPaidOrders(**kwargs)
+    def loadPayments(self, **kwargs):
+        payments = super().loadPayments(**kwargs)
 
         grouped_orders = defaultdict(list)
-        for order in orders:
-            meta_data = order.event.meta_data
+        for payment in payments:
+            meta_data = payment.order.event.meta_data
             pspelement = meta_data['PSP'] if 'PSP' in meta_data else None
-            card_type = self.getCardType(order)
-            order_id = DIBS.get_order_id(order)
+            card_type = self.getCardType(payment)
+            order_id = DIBS.get_order_id(payment)
 
             # Create two lines for double-entry bookkeeping and group orders by
             # artskonto, psp-element, cardtype and order id.
-            grouped_orders[(self.debit_artskonto, None, card_type, order_id)].append(order)
-            grouped_orders[(self.credit_artskonto, pspelement, None, order_id)].append(order)
+            grouped_orders[(self.debit_artskonto, None, card_type, order_id)].append(payment)
+            grouped_orders[(self.credit_artskonto, pspelement, None, order_id)].append(payment)
 
         return grouped_orders
 
-    def loadRefundedOrders(self, **kwargs):
-        orders = super().loadRefundedOrders(**kwargs)
+    def loadRefunds(self, **kwargs):
+        refunds = super().loadRefunds(**kwargs)
 
         grouped_orders = defaultdict(list)
-        for order in orders:
-            meta_data = order.event.meta_data
+        for refund in refunds:
+            meta_data = refund.order.event.meta_data
             pspelement = meta_data['PSP'] if 'PSP' in meta_data else None
-            card_type = self.getCardType(order)
-            order_id = DIBS.get_order_id(order)
+            card_type = self.getCardType(refund.payment)
+            order_id = DIBS.get_order_id(refund.payment)
 
             # Create two lines for double-entry bookkeeping and group orders by
             # artskonto, psp-element, cardtype and order id.
-            grouped_orders[(self.debit_artskonto, None, card_type, order_id)].append(order)
-            grouped_orders[(self.credit_artskonto, pspelement, None, order_id)].append(order)
+            grouped_orders[(self.debit_artskonto, None, card_type, order_id)].append(refund)
+            grouped_orders[(self.credit_artskonto, pspelement, None, order_id)].append(refund)
 
         return grouped_orders
 
-    def formatData(self, paid_orders, refunded_orders, cash_orders, **kwargs):
+    def formatData(self, payments, refunds, cash_payments, **kwargs):
         rows = []
         rows.append(self.headers)
 
-        for [artskonto, pspelement, card_type, order_id], orders in paid_orders.items():
-            amount = sum([order.total for order in orders])
+        for [artskonto, pspelement, card_type, order_id], payments in payments.items():
+            amount = sum([payment.amount for payment in payments])
 
             row = [None] * len(self.headers)
             row[self.index_artskonto] = artskonto
             row[self.index_pspelement] = pspelement
             row[self.index_debit_credit] = 'kredit' if pspelement is not None else 'debet'
             row[self.index_amount] = self.formatAmount(amount)
-            order_ids = ', '.join([DIBS.get_order_id(order) for order in orders])
+            order_ids = ', '.join([DIBS.get_order_id(payment) for payment in payments])
             if card_type is not None:
                 row[self.index_text] = _('Ticket sale ({card_type}): {order_ids}').format(card_type=self.localizeCardType(card_type), order_ids=order_ids)
             else:
@@ -212,15 +202,15 @@ class PaidOrdersLineExporter(PaidOrdersExporter):
 
             rows.append(row)
 
-        for [artskonto, pspelement, card_type, order_id], orders in refunded_orders.items():
-            amount = sum([order.total for order in orders])
+        for [artskonto, pspelement, card_type, order_id], refunds in refunds.items():
+            amount = sum([refund.amount for refund in refunds])
 
             row = [None] * len(self.headers)
             row[self.index_artskonto] = artskonto
             row[self.index_pspelement] = pspelement
             row[self.index_debit_credit] = 'debet' if pspelement is not None else 'kredit'
             row[self.index_amount] = self.formatAmount(amount)
-            order_ids = ', '.join([DIBS.get_order_id(order) for order in orders])
+            order_ids = ', '.join([DIBS.get_order_id(refund.payment) for refund in refunds])
             if card_type is not None:
                 row[self.index_text] = _('Ticket refund ({card_type}): {order_ids}').format(card_type=self.localizeCardType(card_type), order_ids=order_ids)
             else:
@@ -228,15 +218,15 @@ class PaidOrdersLineExporter(PaidOrdersExporter):
 
             rows.append(row)
 
-        for order in cash_orders:
-            meta_data = order.event.meta_data
+        for payment in cash_payments:
+            meta_data = payment.order.event.meta_data
             pspelement = meta_data['PSP'] if 'PSP' in meta_data else None
 
             row = [None] * len(self.headers)
             row[self.index_artskonto] = self.cash_artskonto
             row[self.index_debit_credit] = 'debet'
-            row[self.index_amount] = self.formatAmount(order.total)
-            row[self.index_text] = _('Cash payment ({user}): {order_id}').format(order_id=DIBS.get_order_id(order), user=order.email)
+            row[self.index_amount] = self.formatAmount(payment.order.total)
+            row[self.index_text] = _('Cash payment ({user}): {order_id}').format(order_id=DIBS.get_order_id(payment), user=payment.order.email)
 
             rows.append(row)
 
