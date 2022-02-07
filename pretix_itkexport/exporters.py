598ac14c4e231a394025b72fa4f2a30e00c6e319
Raphael Michel <michel@rami.io>
2022-02-07 17:10:11 +0100

    Remove additional unsued code

--- pretix_itkexport/exporters.py
+++ pretix_itkexport/exporters.py
@@ -55,42 +55,6 @@ class Exporter():
         return paid_orders
 
 
-class EventExporter(Exporter):
-    def getData(self, **kwargs):
-        order_filter = {
-            'event__organizer__in': self.organizers,
-            'status': Order.STATUS_PAID
-        }
-        if 'starttime' in kwargs:
-            order_filter['datetime__gte'] = kwargs['starttime']
-        if 'endtime' in kwargs:
-            order_filter['datetime__lt'] = kwargs['endtime']
-
-        orders = Order.objects.filter(**order_filter).order_by('datetime')
-
-        grouped_orders = defaultdict(list)
-        for order in orders:
-            grouped_orders[order.event].append(order)
-
-        data = []
-
-        for event, orders in grouped_orders.items():
-            revenue = sum([order.total for order in orders])
-            expenses = 0.0
-            audience = event.meta_data['Audience'] if 'Audience' in event.meta_data else None
-
-            data.append({
-                'organizer': event.organizer,
-                'name': event.name,
-                'datetime': event.date_from,
-                'revenue': revenue,
-                'expenses': expenses,
-                'audience': audience
-            })
-
-        return data
-
-
 class PaidOrdersExporter(Exporter):
     """
     Exports paid orders.
