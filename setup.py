9f830b8c0fffa622f57939143f9e678c5ac00de7
Raphael Michel <michel@rami.io>
2022-02-07 17:00:09 +0100

    Declare dependency on `dateparser`

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author_email='rimi@aarhus.dk',
     license='Apache Software License',
 
-    install_requires=[],
+    install_requires=['dateparser'],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     cmdclass=cmdclass,
